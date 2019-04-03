af2e34fc94b0e972f7ef7132a1f48b36d01cbe8b
Cody <25696645+cpolera@users.noreply.github.com>
2019-04-02 18:17:09 -0700

    Initial Commit

---
+++ Assessment.py
@@ -0,0 +1,13 @@
+import unittest
+
+class Assessment:
+
+    def __init__(self):
+        print("blah")
+
+    def test(self):
+        print("somthing")
+
+if __name__ == "__main__":
+    a = Assessment()
+    a.test()
