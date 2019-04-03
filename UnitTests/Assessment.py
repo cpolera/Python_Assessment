7a31c2f8dcd8725dcf87c61b28cbbbb9f1c10b08
Cody <25696645+cpolera@users.noreply.github.com>
2019-04-02 19:27:01 -0700

    Initial Commit

--- UnitTests/Assessment.py
+++ UnitTests/Assessment.py
@@ -1,13 +1,49 @@
 import unittest
+import requests
+import json
 
-class Assessment:
+class Assessment(unittest.TestCase):
 
-    def __init__(self):
-        print("blah")
+    http = "https://"
+    page = "imagequix-qa-interview2"
+    domain = ".herokuapp.com"
+    url = http + page + domain
+    blog_posts = "/blog-posts"
+    authors = "/authors"
 
-    def test(self):
-        print("somthing")
+    def testSum_ConfirmTestWorks(self):
+        assert sum([1,2,3]) ==6, "blahhhh"
+        print(str(sum([1,2,3])) + ' was correct')
+
+    def test_response(self):
+        resp = requests.get(self.url + self.blog_posts)
+        testJson = json.loads(resp.content)
+        print(resp.content)
+        print(testJson[0]["id"])
+
+    def test_response_Author(self):
+        resp = requests.get(self.url + self.authors)
+        testJson = json.loads(resp.content)
+        print(resp.content)
+        for author in testJson:
+            print(author["id"])
+            for val in author:
+                print(val + ":  " + str(author[val]))
+
+    def test_response_AuthorViaID(self):
+        resp = requests.get(self.url + self.authors + "/1")
+        testJson = json.loads(resp.content)
+        print(resp.content)
+        for val in testJson:
+            print(val + ":  " + str(testJson[val]))
+
+    @classmethod
+    def setUp(self):
+        print("Begin test")
 
 if __name__ == "__main__":
-    a = Assessment()
-    a.test()
+    unittest.main()
+    # a = Assessment()
+    # a.testSum_ConfirmTestWorks()
+    # a.test_response()
+    # a.test_response_Author()
