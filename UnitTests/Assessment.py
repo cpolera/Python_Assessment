4ffd9a2b4e7a394b2b7450c7d4a4b6c5e2e9c290
Cody <25696645+cpolera@users.noreply.github.com>
2019-04-02 20:33:22 -0700

    Initial Commit

--- UnitTests/Assessment.py
+++ UnitTests/Assessment.py
@@ -1,25 +1,46 @@
 import unittest
 import requests
 import json
+import datetime
 
 class Assessment(unittest.TestCase):
-
     http = "https://"
     page = "imagequix-qa-interview2"
     domain = ".herokuapp.com"
     url = http + page + domain
     blog_posts = "/blog-posts"
     authors = "/authors"
+    authorPostDict = {"firstName": "", "lastName": ""}
+    blogPostDict = {"title":"", "body":"", "authorId":"", "publishDate":""}
 
-    def testSum_ConfirmTestWorks(self):
-        assert sum([1,2,3]) ==6, "blahhhh"
-        print(str(sum([1,2,3])) + ' was correct')
+    def createAuthor(self):
+        author = ["Cody", "Polera"]
+        newDict = self.authorPostDict.copy()
+        for param in newDict:
+            newDict[param] = author.pop(0)
+        resp = requests.post(Assessment.url+self.authors, newDict)
+        print(resp.content)
+
+    def createBlogPost(self):
+        blog = ["Some title", "Polera", 2001, datetime.datetime.now().isoformat()
+]
+        newDict = self.blogPostDict.copy()
+        for param in newDict:
+            newDict[param] = blog.pop(0)
+        resp = requests.post(Assessment.url + self.blog_posts, json=newDict)
+        print(resp.content)
+
+    def putBlog(self):
+        blog = {"id":1000}
 
     def test_response(self):
         resp = requests.get(self.url + self.blog_posts)
+        expectedId =939
         testJson = json.loads(resp.content)
         print(resp.content)
         print(testJson[0]["id"])
+        self.assertEqual(testJson[0]["id"], expectedId, "Id: " + str(testJson[0]["id"]) + " did not match expected ID: "+ str(expectedId))
+
 
     def test_response_Author(self):
         resp = requests.get(self.url + self.authors)
@@ -39,11 +60,13 @@ class Assessment(unittest.TestCase):
 
     @classmethod
     def setUp(self):
+        print("=======================")
         print("Begin test")
+        print("=======================")
+        self.createBlogPost(self)
+
 
 if __name__ == "__main__":
     unittest.main()
-    # a = Assessment()
-    # a.testSum_ConfirmTestWorks()
-    # a.test_response()
-    # a.test_response_Author()
+
+
